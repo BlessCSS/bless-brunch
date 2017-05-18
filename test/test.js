91d61399543587ebb80059da1bc5d88fbd64eab5
Thomas Conner <thomas@kinvey.com>
2017-05-18 09:39:14 -0400

    Add tests

---
+++ test/test.js
@@ -0,0 +1,30 @@
+const Plugin = require('../');
+const fs = require('fs-extra');
+const path = require('path');
+const expect = require('expect');
+
+describe('Plugin', function() {
+  it('should bless a css file that is to large', function(done) {
+    const plugin = new Plugin({
+      plugins: {
+        bless: {
+          cacheBuster: false,
+          outputDirectory: path.resolve(__dirname, './fixtures/output')
+        }
+      }
+    });
+    plugin.onCompile({
+      path: path.resolve(__dirname, './fixtures/input/over-limit.css')
+    }, () => {
+      expect(fs.existsSync(path.resolve(__dirname, './fixtures/output/over-limit.css'))).toEqual(true);
+      expect(fs.readFileSync(path.resolve(__dirname, './fixtures/output/over-limit.css'), 'utf8').toString())
+        .toEqual(fs.readFileSync(path.resolve(__dirname, './fixtures/expectation/over-limit.css')).toString());
+
+      expect(fs.existsSync(path.resolve(__dirname, './fixtures/output/over-limit-blessed1.css'))).toEqual(true);
+      expect(fs.readFileSync(path.resolve(__dirname, './fixtures/output/over-limit-blessed1.css'), 'utf8').toString())
+        .toEqual(fs.readFileSync(path.resolve(__dirname, './fixtures/expectation/over-limit-blessed1.css')).toString());
+
+      done();
+    });
+  });
+});
